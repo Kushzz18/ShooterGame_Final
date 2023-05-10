89d2723d3cae4d8f4bb6788b3012ea99f7311a23
Kushzz18 <kushalpathak18@gmail.com>
2023-05-10 19:27:13 +0545

    Update

---
+++ Game.java
@@ -0,0 +1,18 @@
+package FirstGame;
+
+import javax.swing.JFrame;
+
+public class Game {
+    public Game() {
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Shooting Game");
+        window.setDefaultCloseOperation(3);
+        window.pack();
+        window.setContentPane(new GamePanel());
+        window.setResizable(false);
+        window.setVisible(true);
+        window.pack();
+    }
+}
